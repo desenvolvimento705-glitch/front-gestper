0352f2d5bc48f14180320fe45c5d3648eb48eaaf
Laboratório 203 <lab203@fiemguba.srv>
2026-06-11 21:07:59 -0300

    teste2

--- javascript/login.js
+++ javascript/login.js
@@ -8,7 +8,7 @@ async function efetuarLogin() {
     }
 
     try{
-        const response = await fetch('https://backgestper-production.up.railway.app/autenticacao/index', {
+        const response = await fetch('https://backgestper-production.up.railway.app/autenticacao/login', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({email, password})
