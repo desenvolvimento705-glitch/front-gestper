7ee9f59c405565a0c1d67b1ee115a34256500ed5
desenvolvimento705-glitch <desenvolvimento705@gmail.com>
2026-07-18 16:58:13 -0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -8,7 +8,7 @@ async function efetuarLogin() {
     }
 
    try {
-    const response = await fetch('https://back-gestper-production.up.railway.app/usuario', {
+    const response = await fetch('https://back-gestper-production.up.railway.app/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({email, password})
